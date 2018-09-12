8340e5a614750e4bb18e50d1674b231dec37295a
tjark.onnen <tjark.onnen@frs.de>
2018-09-12 23:38:35 +0200

    Added Serialization support

---
+++ Soduko.Serialization/SudokuSerializer.cs
@@ -0,0 +1,26 @@
+﻿using DE.Onnen.Sudoku;
+using DE.Onnen.Sudoku.SolveTechniques;
+using System;
+using System.Linq;
+
+namespace Soduko.Serialization
+{
+    public static class SudokuSerializer
+    {
+        public static string GetJson(this Board board, int cellid, int digit)
+        {
+            int[] actual = board.CreateSimpleBoard();
+            string sLine = String.Join(',', actual);
+            return $"{{{Environment.NewLine}\t\"cells\": [{sLine}],{Environment.NewLine}\t\"setdigit\": {{ \"id\": {cellid}, \"digit\": {digit} }}";
+        }
+
+        public static Board ParseToBoard(string json, params ASolveTechnique[] solveTechniques)
+        {
+            int firstBrck = json.IndexOf('[');
+            int lastBrck = json.IndexOf(']');
+            string cv = json.Substring(firstBrck + 1, lastBrck - firstBrck - 1);
+            var cellval = cv.Split(',').Select(x => int.Parse(x.Trim())).ToArray();
+            return new Board(cellval, solveTechniques);
+        }
+    }
+}
---
+++ Sudoku.Test/Serialization/SerializerTest.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// 
+/// </summary>
+namespace Sudoku.Test.Serialization
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using DE.Onnen.Sudoku;
+    using DE.Onnen.Sudoku.SolveTechniques;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Soduko.Serialization;
+
+    [TestClass]
+    public class SerializerTest
+    {
+        private static ASolveTechnique[] _solveTechniques;
+        private Board _board;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._board = new Board(_solveTechniques);
+        }
+
+        [ClassInitialize]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            _solveTechniques = new ASolveTechnique[]
+                {
+                new DE.Onnen.Sudoku.SolveTechniques.HiddenPairTripleQuad(),
+                new DE.Onnen.Sudoku.SolveTechniques.LockedCandidates(),
+                new DE.Onnen.Sudoku.SolveTechniques.NakedPairTrippleQuad()
+                };
+        }
+
+        [TestMethod]
+        public void Serialize_Board_Test()
+        {
+            this._board.SetDigit(0, 1);
+            string json = this._board.GetJson(1, 2);
+            Board tmpBoard = SudokuSerializer.ParseToBoard(json);
+            Assert.AreEqual(1, tmpBoard[0].Digit);
+        }
+    }
+}
